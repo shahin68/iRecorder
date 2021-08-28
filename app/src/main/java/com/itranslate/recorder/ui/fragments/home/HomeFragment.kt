8b54ea66b93dda7b4bc2caa4381cde9efe17fc57
shahin68 <shahin.montazeri@gmail.com>
2021-08-29 01:07:41 +0430

    Layout adjustments in fragment_home.xml & setting click listeners
    Set a size of 120dp & Added ripple by 60dp radius to mic button
    
    Changed btn_home_show_recordings to MaterialButton and added elevation in style

--- app/src/main/java/com/itranslate/recorder/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/itranslate/recorder/ui/fragments/home/HomeFragment.kt
@@ -3,7 +3,9 @@ package com.itranslate.recorder.ui.fragments.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.itranslate.recorder.databinding.FragmentHomeBinding
+import com.itranslate.recorder.general.extensions.onClick
 import com.itranslate.recorder.ui.fragments.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +17,23 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        prepareClickListeners()
+
+    }
+
+    /**
+     * Function to prepare all click listeners
+     */
+    private fun prepareClickListeners() {
+        binding.btnHomeMic.onClick {
+
+        }
+
+        binding.btnHomeShowRecordings.onClick {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToRecordingsFragment()
+            )
+        }
     }
 
 }
