5b91f275a246e1e66c06701716c5ce91a89a4442
shahin68 <shahin.montazeri@gmail.com>
2021-08-29 04:24:52 +0430

    Implementing RecordsDao on LocalRepository
    Providing access to records dao from local repository

--- app/src/main/java/com/itranslate/recorder/data/local/sources/LocalRepositoryImpl.kt
+++ app/src/main/java/com/itranslate/recorder/data/local/sources/LocalRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.itranslate.recorder.data.local.sources
 
+import androidx.paging.DataSource
+import com.itranslate.recorder.data.local.models.records.Record
 import com.itranslate.recorder.data.local.sources.db.records.RecordsDao
 import javax.inject.Inject
 
@@ -14,7 +16,45 @@ import javax.inject.Inject
 class LocalRepositoryImpl @Inject constructor(
     private val recordsDao: RecordsDao
 ): LocalRepository {
+    override suspend fun insertRecord(record: Record) {
+        return recordsDao.insertRecord(record)
+    }
 
+    override suspend fun insertRecord(records: List<Record>) {
+        return recordsDao.insertRecord(records)
+    }
+
+    override suspend fun updateRecord(record: Record) {
+        return recordsDao.updateRecord(record)
+    }
+
+    override suspend fun deleteRecord(record: Record) {
+        return recordsDao.deleteRecord(record)
+    }
+
+    override suspend fun deleteRecordsByRecordId(recordId: Int) {
+        return recordsDao.deleteRecordsByRecordId(recordId)
+    }
+
+    override suspend fun clearAllRecords() {
+        return recordsDao.clearAllRecords()
+    }
+
+    override fun getRecords(): DataSource.Factory<Int, Record> {
+        return recordsDao.getRecords()
+    }
+
+    override fun findRecordsByQuery(query: String): DataSource.Factory<Int, Record> {
+        return recordsDao.findRecordsByQuery(query)
+    }
+
+    override fun getSortedRecords(): DataSource.Factory<Int, Record> {
+        return recordsDao.getSortedRecords()
+    }
+
+    override fun findSortedRecordsByQuery(query: String): DataSource.Factory<Int, Record> {
+        return recordsDao.findSortedRecordsByQuery(query)
+    }
 
 
 }
