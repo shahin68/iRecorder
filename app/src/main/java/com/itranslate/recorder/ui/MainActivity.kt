e06c974af629c3f12d5a37be8b30be5d028906be
shahin68 <shahin.montazeri@gmail.com>
2021-08-31 04:58:03 +0430

    Set actionbar title based on nav controller destination

--- app/src/main/java/com/itranslate/recorder/ui/MainActivity.kt
+++ app/src/main/java/com/itranslate/recorder/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.itranslate.recorder.ui
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.itranslate.recorder.R
 import com.itranslate.recorder.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
@@ -8,6 +10,29 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        addNavControllerDestinationListener()
+
+    }
+
+    /**
+     * Register listener for navigation controller
+     */
+    private fun addNavControllerDestinationListener() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_navigation_host) as NavHostFragment
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> setActionbarTitle(getString(R.string.app_name))
+                R.id.recordingsFragment -> setActionbarTitle(getString(R.string.actionbar_title_recordings))
+            }
+        }
+    }
+
+    /**
+     * set action bar title
+     */
+    private fun setActionbarTitle(string: String) {
+        supportActionBar?.title = string
     }
 
 }
