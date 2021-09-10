6f61d87e629c372b55cd277a81de17c3e7328398
shahin68 <shahin.montazeri@gmail.com>
2021-09-10 15:46:29 +0430

    Removing unnecessary code from ViewExtensions.kt
    In support of clean code, when a line of code has not been used anywhere
    it shouldn't exist at all

--- app/src/main/java/com/itranslate/recorder/general/extensions/ViewExtensions.kt
+++ app/src/main/java/com/itranslate/recorder/general/extensions/ViewExtensions.kt
@@ -1,7 +1,6 @@
 package com.itranslate.recorder.general.extensions
 
 import android.view.View
-import androidx.core.view.isVisible
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.google.android.material.progressindicator.LinearProgressIndicator
 
@@ -20,13 +19,6 @@ fun <T: View> T.gone() {
 }
 
 /**
- * extension function to set view visibility as INVISIBLE
- */
-fun <T: View> T.hide() {
-    visibility = View.INVISIBLE
-}
-
-/**
  * extension function to set view visibility as VISIBLE or GONE
  */
 fun <T: View> T.visibleOrGone(visible: Boolean) {
@@ -36,29 +28,6 @@ fun <T: View> T.visibleOrGone(visible: Boolean) {
         gone()
     }
 }
-
-/**
- * extension function to toggle view visibility
- */
-fun <T: View> T.toggleVisibleOrGone() {
-    if (isVisible) {
-        gone()
-    } else {
-        visible()
-    }
-}
-
-/**
- * extension function to set view visibility as VISIBLE or INVISIBLE
- */
-fun <T: View> T.visibleOrHide(show: Boolean) {
-    if (show) {
-        visible()
-    } else {
-        hide()
-    }
-}
-
 /**
  * extension function to register click listener on a view
  */
