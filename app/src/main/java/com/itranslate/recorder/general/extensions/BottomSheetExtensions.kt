6b84cf4e83c3645c20958e2e66c50fcf1b1641c3
shahin68 <shahin.montazeri@gmail.com>
2021-09-10 20:15:25 +0430

    Create extension function for bottom sheet behaviors & callback
    Making the usage of such calls in fragments cleaner

---
+++ app/src/main/java/com/itranslate/recorder/general/extensions/BottomSheetExtensions.kt
@@ -0,0 +1,30 @@
+package com.itranslate.recorder.general.extensions
+
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+/**
+ * Extension function to expand
+ */
+fun <T : BottomSheetBehavior<View>> T.expand() {
+    state = BottomSheetBehavior.STATE_EXPANDED
+}
+
+
+/**
+ * Extension function to register bottom sheet callback
+ * invokes [onCollapseCallback] after bottom sheet has collapsed
+ */
+fun <T : BottomSheetBehavior<View>> T.registerCallback(onCollapseCallback: () -> Unit) {
+    addBottomSheetCallback(object :BottomSheetBehavior.BottomSheetCallback(){
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+            if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                onCollapseCallback.invoke()
+            }
+        }
+
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+        }
+    })
+}
